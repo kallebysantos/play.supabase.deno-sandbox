cfeb86322aa0586254ff278d0d690a0c42234d4a
Kalleby Santos <kalleby_santos@hotmail.com>
2026-08-30 15:42:19 +0100

    perf: refactoring to server routes

--- src/server.ts
+++ src/server.ts
@@ -1,45 +1,78 @@
+// deno-lint-ignore-file require-await
 import { SandboxManager } from './sandbox/manager.ts'
 
-const sandboxManager = new SandboxManager()
+const context = {
+  sandboxManager: new SandboxManager(),
+}
+
+export type ServerCtx = typeof context
+
+export type ServerRoute<T = unknown> = {
+  match: URLPattern
+  fetch: T extends object ? (req: Request, ctx: ServerCtx, values: T) => Promise<Response>
+    : (req: Request, ctx: ServerCtx) => Promise<Response>
+}
+
+export const list: ServerRoute = {
+  match: new URLPattern({ pathname: '/list' }),
+  async fetch(_req, ctx) {
+    const sandboxes = ctx.sandboxManager.list()
+    return Response.json(sandboxes, { status: 201 })
+  },
+}
+
+const create: ServerRoute = {
+  match: new URLPattern({ pathname: '/create' }),
+  async fetch(_req, ctx) {
+    const sandbox = ctx.sandboxManager.create()
+
+    return Response.json(sandbox, { status: 201 })
+  },
+}
+
+const terminate: ServerRoute<{ id: string }> = {
+  match: new URLPattern({ pathname: '/terminate' }),
+  async fetch(_req, ctx, { id }) {
+    ctx.sandboxManager.delete(id)
+
+    return new Response(null, { status: 204 })
+  },
+}
+
+export const run: ServerRoute<{ id: string }> = {
+  match: new URLPattern({ pathname: '/run/:id' }),
+  async fetch(_req, ctx, { id }) {
+    const sandbox = ctx.sandboxManager.get(id)
+    if (!sandbox) return new Response(null, { status: 404 })
+
+    sandbox.run(`console.log('Hello from Sandbox')`)
+
+    return new Response(null, { status: 200 })
+  },
+}
 
 export default {
-  fetch: (req, info) => {
-    console.log(info, req)
-    const url = new URL(req.url)
-
-    if (url.pathname.startsWith('/list')) {
-      const sandboxes = sandboxManager.list()
-
-      return Response.json(sandboxes, { status: 201 })
+  fetch: (req) => {
+    const isList = list.match.exec(req.url)
+    if (isList) {
+      return list.fetch(req, context)
     }
 
-    if (url.pathname.startsWith('/create')) {
-      const sandbox = sandboxManager.create()
-
-      return Response.json(sandbox, { status: 201 })
+    const isCreate = create.match.exec(req.url)
+    if (isCreate) {
+      return create.fetch(req, context)
     }
 
-    if (url.pathname.startsWith('/run')) {
-      const id = url.pathname.split('/').at(2)
-      if (!id) return new Response('missing id', { status: 400 })
-
-      const sandbox = sandboxManager.get(id)
-      if (!sandbox) return new Response(null, { status: 404 })
-
-      sandbox.run(`console.log('Hello from Sandbox')`)
-
-      return new Response(null, { status: 200 })
+    const isRun = run.match.exec(req.url)
+    if (isRun) {
+      return run.fetch(req, context, { id: isRun.pathname.groups.id! })
     }
 
-    if (url.pathname.startsWith('/terminate')) {
-      const id = url.pathname.split('/').at(2)
-      if (!id) return new Response('missing id', { status: 400 })
-
-      sandboxManager.delete(id)
-
-      return new Response(null, { status: 204 })
+    const isTerminate = terminate.match.exec(req.url)
+    if (isTerminate) {
+      return terminate.fetch(req, context, { id: isTerminate.pathname.groups.id! })
     }
 
-    return new Response()
+    return new Response('Action NotFound', { status: 404 })
   },
 } satisfies Deno.ServeDefaultExport
